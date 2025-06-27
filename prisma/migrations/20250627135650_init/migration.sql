85f589b2a8e1c5f639e6c1c79528c05ea3a96c26
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2025-06-27 15:08:25 +0100

    Basic Auth

---
+++ prisma/migrations/20250627135650_init/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ProductImage` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `admin` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `googleId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Product";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "ProductImage";
+PRAGMA foreign_keys=on;
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT
+);
+INSERT INTO "new_User" ("email", "id", "password", "username") SELECT "email", "id", "password", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response , Application } from 'express';
+import { PrismaClient } from '../generated/prisma'
+import dotenv from 'dotenv';
+import authRouter from './routes/auth';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+app.use(express.json());
+
+export const prisma = new PrismaClient();
+
+app.use('/auth', authRouter);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Home');
+});
+
+app.listen(port, () => {
+    console.log(`Server @ http://localhost:${port}`);
+});
